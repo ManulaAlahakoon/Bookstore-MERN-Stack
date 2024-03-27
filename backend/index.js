502d9634696e743e99c606b3d74f3260145158be
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-27 12:36:38 +0530

    Saving a new book in the database

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,40 @@
 //const express = require('express')
 import express from 'express'
 import mongoose from 'mongoose'
-import {PORT,mongodbURL} from './config.js'
+import { PORT, mongodbURL } from './config.js'
+import {Book} from './models/bookModel.js'
 
 const app = express()
+app.use(express.json())
 
 const port = process.env.PORT || PORT
 
 app.get('/', (req, res) => {
     console.log(req)
     return  res.status(200).send("Lets learn MERN stack")
+})
+
+app.post('/books', async (req,res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishYear){
+            res.status(400).send(
+                {message:'Please send all required fields'}
+            )
+        } 
+
+        const newBook = { 
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        }
+
+        const book = await Book.create(newBook)
+        res.status(200).send(book) 
+        
+    } catch (error) { // Errors are not showing as a message
+        console.log(error.message)
+        return res.status(500).send({message: error.message})
+    }
 })
 
 mongoose
