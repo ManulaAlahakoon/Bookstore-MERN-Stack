1d5e2cea5526df1aedd2cf471c5fe597e33786c5
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-27 09:07:56 +0530

    Creating first Http route
    Get request

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,11 @@ const app = express()
 
 const port = process.env.PORT || PORT
 
+app.get('/', (req, res) => {
+    console.log(req)
+    return  res.status(200).send("Lets learn MERN stack")
+})
+
 app.listen(port, () => {
     console.log(`App is listening to port ${port}`)
 })
