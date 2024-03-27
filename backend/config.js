3edccd0254020fd8214093a7b1ccc4023d74a2e3
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-27 11:44:59 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
-export const mongodbURL = 'mongodb+srv://admin:y8OIsaLYxsZH2T4v@cluster0.adddaby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+export const mongodbURL = 'mongodb+srv://admin:y8OIsaLYxsZH2T4v@cluster0.adddaby.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Cluster0'
 
