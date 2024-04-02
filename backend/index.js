68d14cfc6343988156ee84bc98eae6b9f6a638b3
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-04-02 08:02:07 +0530

    Making a custom origins cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 //const express = require('express')
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 import { PORT, mongodbURL } from './config.js'
 import { Book } from './models/bookModel.js'
 import booksRoute from './routes/bookRoutes.js'
@@ -8,7 +9,17 @@ import booksRoute from './routes/bookRoutes.js'
 
 const app = express()
 app.use(express.json())
-app.use('/books',booksRoute)
+app.use('/books', booksRoute)
+
+//Middleware for handling CORS Policy
+//Option 1 - Allow all origins with default of cors
+//app.use(cors())
+//Option 2 - Allow custom origins
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
 
 const port = process.env.PORT || PORT
 
